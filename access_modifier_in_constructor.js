6f386ffa139e499b5c919a256824bc50fe7d4660
Souma <souma.hitech@gmail.com>
2021-03-09 00:02:06 +0530

    Complete getter and setter and properties

--- access_modifier_in_constructor.js
+++ access_modifier_in_constructor.js
@@ -10,7 +10,29 @@ var Point = /** @class */ (function () {
     };
     Point.prototype.getDistance = function (another) {
     };
+    Object.defineProperty(Point.prototype, "X", {
+        get: function () {
+            return this.x;
+        },
+        set: function (value) {
+            if (value < 0)
+                throw new Error("Value can't be less than zero");
+            this.x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Point.prototype.getY = function () {
+        return this.y;
+    };
+    Point.prototype.setY = function (value) {
+        if (value < 0)
+            throw new Error("Value can't be less than zero");
+        this.y = value;
+    };
     return Point;
 }());
 var point = new Point(1, 2);
+point.X = 10;
+point.setY(20);
 point.draw();
--- access_modifier_in_constructor.ts
+++ access_modifier_in_constructor.ts
@@ -1,21 +1,47 @@
 class Point {
 
     // access modifier in  constructor parameter 
-    constructor(private x?: number, private y?: number) {
+    constructor(private _x?: number, private _y?: number) {
 
     }
 
     draw() {
         console.log("Drawing...");
-        console.log('X: ' + this.x + ', y: ' + this.y);
+        console.log('X: ' + this._x + ', y: ' + this._y);
     }
 
     getDistance(another: Point) {
 
     }
+
+    // properties
+    get x(){
+        return this._x;
+    }
+
+    // getter 
+    getY(){
+        return this._y;
+    }
+
+    //properties
+    set x(value: number) {
+        if (value < 0) 
+            throw new Error("Value can't be less than zero");
+        this._x = value;
+    }
+
+    // setter
+    setY(value: number) {
+        if (value < 0) 
+            throw new Error("Value can't be less than zero");
+        this._y = value;
+    }
 }
 
 
 let point: Point = new Point(1, 2);
+point.x = 10;
+point.setY(20);
 point.draw();
 
