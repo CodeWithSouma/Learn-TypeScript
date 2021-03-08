31e8fd4d4bc8a64bfdbb7809fceb71a11b55f0f7
Souma <souma.hitech@gmail.com>
2021-03-08 23:14:43 +0530

    complete interface

---
+++ interface.js
@@ -0,0 +1,10 @@
+var drawPoint = function (point) {
+    //...
+};
+var getDistance = function (pointA, pointB) {
+    // ...
+};
+drawPoint({
+    x: 1,
+    y: 2
+});
---
+++ interface.ts
@@ -0,0 +1,21 @@
+interface Point{
+    x: number, 
+    y: number, 
+    draw: () => void
+}
+
+let drawPoint = (point : {x: number, y: number}) => {
+    //...
+}
+
+let getDistance = (pointA: number, pointB: number ) => {
+    // ...
+}
+
+drawPoint({
+    x: 1,
+    y: 2
+
+});
+
+
