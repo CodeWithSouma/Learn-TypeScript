ad663db9cd5692a1dbb0d6e0eb4890037bb7ec29
Souma <souma.hitech@gmail.com>
2021-03-08 21:31:19 +0530

    Complete var and let in ts

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function log(message) {
 }
 var message = 'Hello World!';
 log(message);
+function doSomething() {
+    for (var i = 1; i < 5; i++)
+        console.log(i);
+    console.log('Finally: ' + i);
+}
+doSomething();
--- main.ts
+++ main.ts
@@ -4,3 +4,12 @@ function log(message){
 
 var message = 'Hello World!';
 log(message);
+
+
+function doSomething(){
+    for(var i = 1; i < 5; i++ )
+        console.log(i);
+    console.log('Finally: '+i);
+}
+
+doSomething();
