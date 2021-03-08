d4aa9ac27b9f9293641fa2635dc313fd9cfa661a
Souma <souma.hitech@gmail.com>
2021-03-08 23:39:56 +0530

    complete access modifier in tsc

--- class.ts
+++ class.ts
@@ -1,25 +1,25 @@
-class Point{
-    x: number; 
-    y: number; 
+class Point {
+    private x: number; // we can't access this form outside of the class
+    private y: number;
 
-    constructor(x?: number, y?: number){
+    constructor(x?: number, y?: number) {
         this.x = x;
         this.y = y;
     }
-    
-    draw(){
+
+    draw() {
         console.log("Drawing...");
-        console.log('X: '+this.x + ', y: '+this.y);
+        console.log('X: ' + this.x + ', y: ' + this.y);
     }
 
-    getDistance(another: Point){
+    getDistance(another: Point) {
 
     }
 }
 
 
-let point:Point = new Point(1,2);
+let point: Point = new Point(1, 2);
 point.draw();
 
-let point2:Point = new Point();
+let point2: Point = new Point();
 point2.draw();
