c5b624f236602d69e96daf83c6e8bdd31edd8e82
Souma <souma.hitech@gmail.com>
2021-03-08 22:06:51 +0530

    Complete arrow function in typescript

---
+++ arrow_function.js
@@ -0,0 +1,6 @@
+// in javascript 
+var log = function (message) {
+    console.log(message);
+};
+// in typescript we have arrow function
+var doLog = function (message) { return log(message); };
---
+++ arrow_function.ts
@@ -0,0 +1,8 @@
+// in javascript 
+let log = function(message){
+    console.log(message);
+    
+}
+
+// in typescript we have arrow function
+let doLog = message => log(message);
